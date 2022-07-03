d2da7b5383cf753201f138e654c0fec482652eb8
Afag Gadimova <afagadimova536@gmail.com>
2022-07-03 15:10:45 -0700

    Initial commit

---
+++ src/test/java/TeamFramework/BaseClass.java
@@ -0,0 +1,41 @@
+package TeamFramework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseClass {
+
+
+
+    public WebDriver driver;
+    @BeforeClass
+    public void methodSetUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+
+    public void setUpMethod() {
+        driver = new ChromeDriver();
+        driver.get("https://www.delta.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod()
+
+
+    public void afterMessage() {
+        driver.quit();
+    }
+}
+
+
+
