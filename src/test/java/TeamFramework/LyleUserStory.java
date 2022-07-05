f2ca9b18aec4f149ea4f6c27e73671cc4c094051
Lyle Bantilo <lylebcastos@gmail.com>
2022-07-05 19:59:56 -0400

    final commit

--- src/test/java/TeamFramework/LyleUserStory.java
+++ src/test/java/TeamFramework/LyleUserStory.java
@@ -21,7 +21,7 @@ public class LyleUserStory extends BaseClass{
         driver.findElement(By.xpath("//li[@class='airport-list ng-star-inserted'][1]")).click();
 
     }
-    @Test (groups= "smoke")
+    @Test
     public void RoundTripandFlightDates(){
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//input[@name='fromAirportCode']")));
@@ -31,16 +31,17 @@ public class LyleUserStory extends BaseClass{
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//input[@name='arrivalCity']")));
         driver.findElement(By.xpath("//input[@id='search_input']")).sendKeys("BUR");
         driver.findElement(By.xpath("//li[@class='airport-list ng-star-inserted'][1]")).click();
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//span[@id='selectTripType-val']")));
-        driver.findElement(By.xpath("//span[@id='selectTripType-val'][1]")).click();
-//        driver.findElement(By.xpath("//span[@class='calenderDepartSpan'][1]")).click();
-//        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("selectTripType-val")));
+
+
+        driver.findElement(By.xpath("//span[@aria-labelledby='selectTripType-label']")).click();
+        driver.findElement(By.xpath("//li[@id='ui-list-selectTripType0']")).click();
+
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.id("input_departureDate_1")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//a[@aria-label='15 August 2022, Monday']")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//a[@aria-label='22 August 2022, Monday']")));
 
     }
-    @Test (groups= "smoke")
+    @Test
     public void onePassenger() {
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//input[@name='fromAirportCode']")));
@@ -57,12 +58,11 @@ public class LyleUserStory extends BaseClass{
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//a[@aria-label='15 August 2022, Monday']")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//a[@aria-label='22 August 2022, Monday']")));
 
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//span[@id='passengers-val']")));
-        driver.findElement(By.xpath("//span[@id='passengers-val'][1]")).click();
-
+        driver.findElement(By.xpath("//span[@aria-labelledby='passengers-label']")).click();
+        driver.findElement(By.xpath("//li[text()='1 Passenger']")).click();
 
     }
-    @Test (groups= "smoke")
+    @Test
     public void submitButton() {
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//input[@name='fromAirportCode']")));
